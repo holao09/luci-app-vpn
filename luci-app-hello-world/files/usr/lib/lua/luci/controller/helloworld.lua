5f372e4105b2006c18c12d2315f87656d033bc13
Việt Nguyễn <mrholao09@gmail.com>
2021-06-29 11:17:09 +0700

    Update helloworld.lua

--- luci-app-hello-world/files/usr/lib/lua/luci/controller/helloworld.lua
+++ luci-app-hello-world/files/usr/lib/lua/luci/controller/helloworld.lua
@@ -2,9 +2,8 @@ module("luci.controller.helloworld", package.seeall)  --notice that new_tab is t
 function index()
    entry({"admin", "helloworld"}, firstchild(), "Hello World", 60).dependent=false  --this adds the top level tab and defaults to the first sub-tab (tab_from_cbi), also it is set to position 30
    entry({"admin", "helloworld", "tab_from_cbi"}, cbi("helloworld/cbi_tab"), "CBI Tab", 1)  --this adds the first sub-tab that is located in <luci-path>/luci-myapplication/model/cbi/myapp-mymodule and the file is called cbi_tab.lua, also set to first position
-   entry({"admin", "helloworld", "tab_from_cbi", "click"}, call("action_tryme"))
    entry({"admin", "helloworld", "tab_from_view"}, template("helloworld/view_tab"), "View Tab", 2)  --this adds the second sub-tab that is located in <luci-path>/luci-myapplication/view/myapp-mymodule and the file is called view_tab.htm, also set to the second position
-   
+   entry({"admin", "helloworld", "click"}, call("action_tryme"), "Click here", 10).dependent=false
 end
 
 function action_tryme()
